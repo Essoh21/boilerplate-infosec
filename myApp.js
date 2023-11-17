c02d0dab97a32acd4342348376033ce87d2d9fdc
essohanam <essohanam.alou@imsp-uac.org>
2023-11-17 12:16:50 +0000

    add helmet config

--- myApp.js
+++ myApp.js
@@ -1,59 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const helmet = require("helmet");
 
+/* app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
+ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl()); // this highly reduce performance
+app.use(helmet.noCache()); // highly decrease performance use only when it is really necessary
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
+  })
+); // very important to use and  customize */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// use helmet parent middleware
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
